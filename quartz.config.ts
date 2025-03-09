9406ab647976041ccba98da44bb5553af22ec877
BazzoFX <bazzofx@outlook.com>
2025-03-09 23:40:28 +0000

    Quartz sync: Mar 9, 2025, 11:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,7 +30,7 @@ const config: QuartzConfig = {
           lightgray: "#e5e5e5", 
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
+          dark: "#5f5edf",
           secondary: "#928374",
           tertiary: "#a89984",
           highlight: "rgba(143, 159, 169, 0.15)",
